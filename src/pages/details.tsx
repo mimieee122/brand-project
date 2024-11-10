9cf8f4b9fa9b516dd19d5db2a513eb6b6d5a6432
mimieee122 <giyomii030@naver.com>
2024-11-10 18:18:43 +0900

    8

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -26,28 +26,38 @@ const Details = () => {
                     </Link>
                 </div>
             </div>
-            <div className="w-screen h-[3px] mt-[15px] justify-self-center bg-gray-600"></div>
+            <div className="w-screen h-[2px] mt-[15px] justify-self-center bg-[#836d4c]"></div>
 
             <div className="flex flex-col items-center gap-[10px] mt-[20px]">
                 <div className="flex flex-row w-max-screen justify-between gap-[100px] ml-[140px]">
-                    <div className="flex flex-col shadow-2xl rounded-md mt-[5px] w-[850px] h-[580px] border-black border-[2px] border-solid items-center">
-                        <div className=" relative  flex flex-col gap-0 items-center w-[400px] h-[280px]">
-                            <Image
-                                src="/assets/images/marimo.png"
-                                fill // 부모 요소에 가득 차게 함
-                                alt="회색 이미지"
-                                className="object-fill "
-                            />
+                    <div className="flex flex-col shadow-2xl rounded-md mt-[5px] w-[850px] h-[580px] border-[#544c4c] border-[3px] border-solid items-center">
+                        <div className="flex flex-row justify-center mt-[5px] relative gap-[30px]">
+                            <div className="relative flex flex-col  w-[150px] h-[280px] items-center">
+                                <Image
+                                    src="/assets/images/option.png"
+                                    fill
+                                    alt="items"
+                                    className="object-fill"
+                                />
+                            </div>
+                            <div className=" relative  flex flex-col gap-0 items-center w-[400px] h-[280px]">
+                                <Image
+                                    src="/assets/images/marimo.png"
+                                    fill // 부모 요소에 가득 차게 함
+                                    alt="회색 이미지"
+                                    className="object-fill "
+                                />
+                            </div>
                         </div>
                         <div className="flex flex-col items-center mt-[10px] justify-center brandExplain text-[24px] text-black">
-                            <span className="name2 mb-[2px] text-[52px] text-[#544c4c] underline">
+                            <span className="name2 mb-[2px] text-[50px] text-[#544c4c] underline">
                                 DETAILS
                             </span>
                             <span className="text-[#836d4c]">
                                 원액은 1회분(50ml) 개별 포장되며, 물(200ml)과 약
                                 1:4 비율로 섞으면 됩니다.
                             </span>
-                            <div className="flex box flex-col items-center shadow-2xl pt-[10px] w-[800px] h-[150px] rounded-2xl pl-3 pr-3 border-[1.5px] border-solid border-[#949597] text-[black] mt-[23px] mb-[5px] gap-[5px]">
+                            <div className="flex box flex-col items-center shadow-2xl pt-[10px] w-[800px] h-[140px] rounded-2xl pl-3 pr-3 border-[1.5px] border-solid border-[#949597] text-[black] mt-[13px] mb-[5px] gap-[5px]">
                                 <span>
                                     마르모는 여러분께 전문점 수준의 드립커피를
                                     세상에서 가장
@@ -72,7 +82,7 @@ const Details = () => {
                     </div>
                 </div>
 
-                <div className="w-screen h-[3px] mt-[10px] justify-self-center bg-gray-600"></div>
+                <div className="w-screen h-[2px] mt-[10px] justify-self-center bg-[#836d4c]"></div>
                 <Link href={'/items'}>
                     <div className="relative flex flex-col gap-0 justify-start items-start w-[60px] h-[45px]">
                         <Image
