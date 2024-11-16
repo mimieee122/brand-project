8c1c5c7701596c6a6062100f50276e8c7ab5f14d
mimieee122 <giyomii030@naver.com>
2024-11-16 16:38:11 +0900

    hi

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -28,10 +28,18 @@ const Details = () => {
             <div className="w-screen h-[1.5px] mt-[15px] justify-self-center bg-[#544c4c]"></div>
 
             <div className="flex flex-col items-center gap-[10px] mt-[20px]">
-                <div className="flex flex-row w-max-screen justify-between gap-[100px] ml-[140px]">
-                    <div className="flex flex-col details shadow-2xl rounded-2xl mt-[5px] w-[850px] h-[580px] border-[#6b826d] border-[3px] border-solid items-center">
-                        <div className="flex flex-row justify-center mt-[5px] relative gap-[30px]">
-                            <div className="relative flex flex-col  w-[150px] h-[280px] items-center">
+                <div className="flex flex-row w-max-screen justify-between gap-[100px] ">
+                    <div className="flex flex-col details shadow-2xl rounded-2xl mt-[5px] w-[850px] h-[580px] border-[#6b826d] border-[5px] border-solid items-center">
+                        <div className="flex flex-row mt-[15px] justify-center relative gap-[30px]">
+                            <div className="item relative flex mt-[15px]   w-[250px] h-[230px] flex-col justify-center items-center group">
+                                <Image
+                                    src="/assets/images/guatemala.png"
+                                    fill // 부모 요소에 가득 차게 함
+                                    alt="제품 사진"
+                                    className="object-fill"
+                                />
+                            </div>
+                            <div className="relative flex w-[140px] h-[270px] flex-col justify-center items-center">
                                 <Image
                                     src="/assets/images/option.png"
                                     fill
@@ -39,24 +47,25 @@ const Details = () => {
                                     className="object-fill"
                                 />
                             </div>
-                            <div className=" relative  flex flex-col gap-0 items-center w-[400px] h-[280px]">
+
+                            <div className="item relative flex w-[250px] h-[230px] mt-[15px] flex-col justify-center items-center group ">
                                 <Image
-                                    src="/assets/images/marimo.png"
+                                    src="/assets/images/ethiopia.png"
                                     fill // 부모 요소에 가득 차게 함
-                                    alt="회색 이미지"
-                                    className="object-fill "
+                                    alt="제품 사진"
+                                    className="object-fill"
                                 />
                             </div>
                         </div>
                         <div className="flex flex-col items-center mt-[5px] justify-center brandExplain text-[24px] text-black">
-                            <span className="name2 mb-[2px] text-[55px] text-[#544c4c] underline">
+                            <span className="detail mb-[2px] text-[55px] text-[black] underline">
                                 DETAILS
                             </span>
                             <span className="text-[#6b826d]">
                                 원액은 1회분(50ml) 개별 포장되며, 물(200ml)과 약
                                 1:4 비율로 섞으면 됩니다.
                             </span>
-                            <div className="flex box bg-opacity-80 flex-col items-center shadow-2xl pt-[10px] w-[800px] h-[140px] rounded-2xl pl-3 pr-3 border-[1.5px] border-solid border-[white] text-[black] mt-[13px] mb-[5px] gap-[5px]">
+                            <div className="flex box bg-opacity-80 flex-col items-center shadow-2xl pt-[10px] w-[800px] h-[140px] rounded-2xl pl-3 pr-3 border-[2.5px] border-solid border-[black] text-[black] mt-[7px] mb-[5px] gap-[5px]">
                                 <span>
                                     마르모는 여러분께 전문점 수준의 드립커피를
                                     세상에서 가장
@@ -71,19 +80,19 @@ const Details = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="relative flex flex-col gap-0 mr-0 pr-0 w-[330px] h-[580px]">
+                    {/* <div className="relative flex flex-col gap-0 mr-0 pr-0 w-[330px] h-[580px]">
                         <Image
                             src="/assets/images/drink.png"
                             fill // 부모 요소에 가득 차게 함
                             alt="회색 이미지"
                             className="object-fill "
                         />
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className="w-screen h-[1.5px] mt-[10px] justify-self-center bg-[#544c4c]"></div>
                 <Link href={'/items'}>
-                    <div className="relative flex flex-col gap-0 justify-start items-start w-[60px] h-[45px]">
+                    <div className="relative flex flex-col gap-0 justify-center items-center w-[60px] h-[45px]">
                         <Image
                             src="/assets/images/arrow.png"
                             fill // 부모 요소에 가득 차게 함
